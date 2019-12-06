a84e019526feb2d355a79b38dfe965f5275618cb
fredck <f.knabben@cksource.com>
2019-12-06 15:46:53 +0100

    Removed outdated code, confirmed to not be anymore needed.

--- src/html2markdown/html2markdown.js
+++ src/html2markdown/html2markdown.js
@@ -6,10 +6,6 @@
 import TurndownService from 'turndown';
 import { gfm } from 'turndown-plugin-gfm';
 
-// TODO: Implement converters (now "rules") (if necessary).
-// TODO: Delete the legacy lib/to-markdown directory.
-// import converters from './lib/to-markdown/converters';
-
 const turndownService = new TurndownService( {
 	codeBlockStyle: 'fenced',
 	hr: '---',
@@ -27,12 +23,12 @@ export default function html2markdown( html ) {
 // This is a copy of the original taskListItems rule from turdown-plugin-gfm, with minor changes.
 function todoList( turndownService ) {
 	turndownService.addRule( 'taskListItems', {
-		filter: function( node ) {
+		filter( node ) {
 			return node.type === 'checkbox' &&
 				// Changes here as CKEditor outputs a deeper structure.
 				( node.parentNode.nodeName === 'LI' || node.parentNode.parentNode.nodeName === 'LI' );
 		},
-		replacement: function( content, node ) {
+		replacement( content, node ) {
 			return ( node.checked ? '[x]' : '[ ]' ) + ' ';
 		}
 	} );
